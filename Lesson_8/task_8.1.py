dcbf0155421d87a99d24ceba319b3a9404857562
masterjr <juniormaster@mail.ru>
2020-03-27 00:16:17 +0300

    Updated task 8.1

--- Lesson_8/task_8.1.py
+++ Lesson_8/task_8.1.py
@@ -7,12 +7,12 @@ import hashlib
 
 def substring_find(string):
     assert len(string) > 0
-    hash_list = []
+    hash_list = set()
     iter = 1
     while iter < len(string):
         for i in range(len(string)):
             hash_tmp = hashlib.sha1(string[i:i + iter].encode('utf-8')).hexdigest()
-            hash_list.append(hash_tmp)
+            hash_list.add(hash_tmp)
         iter += 1
     return f'В указанной строке {len(set(hash_list))} различных подстрок'
 
