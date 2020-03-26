c927d2ba13ae67d7903046489f1e447972dc3c55
masterjr <juniormaster@mail.ru>
2020-03-27 00:12:32 +0300

    Added solution of task 8.1

---
+++ Lesson_8/task_8.1.py
@@ -0,0 +1,20 @@
+"""
+Определение количества различных подстрок с использованием хеш-функции. Пусть на вход функции дана строка.
+Требуется вернуть количество различных подстрок в этой строке.
+"""
+
+import hashlib
+
+def substring_find(string):
+    assert len(string) > 0
+    hash_list = []
+    iter = 1
+    while iter < len(string):
+        for i in range(len(string)):
+            hash_tmp = hashlib.sha1(string[i:i + iter].encode('utf-8')).hexdigest()
+            hash_list.append(hash_tmp)
+        iter += 1
+    return f'В указанной строке {len(set(hash_list))} различных подстрок'
+
+
+print(substring_find('a a '))
